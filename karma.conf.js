78ae5134ec18d2781b36082c9ade638984ebc119
Andy Mepham <andymeps@yahoo.com>
2016-08-24 15:48:31 +0100

    Adding initial files.
    Very much a work in progress

---
+++ karma.conf.js
@@ -0,0 +1,30 @@
+var webpackConfig = require('./webpack.config');
+
+module.exports = function(config) {
+  config.set({
+    basePath: '',
+    frameworks: ['mocha', 'chai', 'sinon'],
+    files: [
+      'test/*.ts'
+    ],
+    exclude: [
+      'typings',
+      'node_modules'
+    ],
+    preprocessors: {
+      'test/**/*.ts': ['webpack']
+    },
+    webpack: {
+      module: webpackConfig.module,
+      resolve: webpackConfig.resolve
+    },
+    reporters: ['progress'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['PhantomJS'],
+    singleRun: false,
+    concurrency: Infinity
+  })
+}
